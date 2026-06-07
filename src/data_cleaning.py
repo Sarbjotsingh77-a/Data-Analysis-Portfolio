aeeca6c40c9c5bb9ffdd77c6ed0f905ac0940c6e
Sarbjotsingh77-a <kherarajindersingh94@gmail.com>
2026-06-07 21:23:52 +0530

    Update data_cleaning.py

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,1 +1,14 @@
+import pandas as pd
 
+def check_missing_values(df):
+    return df.isnull().sum()
+
+def remove_duplicates(df):
+    return df.drop_duplicates()
+
+def dataset_summary(df):
+    return {
+        "Rows": df.shape[0],
+        "Columns": df.shape[1],
+        "Missing Values": df.isnull().sum().sum()
+    }
